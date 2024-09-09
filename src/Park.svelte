242a8a0a3258c1ea67d1de7318833ec0ca40c8c6
Jeffrey Hargarten <harga012@umn.edu>
2024-09-09 14:36:57 -0500

    updates

--- src/Park.svelte
+++ src/Park.svelte
@@ -6,7 +6,7 @@
   
   <h4 class="source">Data sources: Minneapolis Park Police</h4>
   
-  <div><p>The Minneapolis Park Police report to the Minneapolis PArk and Recreation Board, work closely with MPD and claim jurisdiction over criminal incidents reported in parks. It was established in 1887 and employs about 30 sworn officers and 20 part-time patrol agents</p></div>
+  <div><p>The Minneapolis Park Police report to the Minneapolis Park and Recreation Board, work closely with MPD and claim jurisdiction over criminal incidents reported in parks. It was established in 1887 and employs about 30 sworn officers and 20 part-time patrol agents</p></div>
   
   <div><p>Park Police data is usually wrapped into MPD data since they collaborate on most things.</p></div>
   
