3bdf29e9f93a00f4beff559b93613198bd0a455e
Jeffrey Hargarten <jeffhargarten3@gmail.com>
2025-04-22 23:17:40 -0500

    updates

--- src/Conduct.svelte
+++ src/Conduct.svelte
@@ -8,12 +8,13 @@
 
 <p>MPD misconduct complaints are overseen by the Office of Police Conduct Review (OPCR). Its data is complex and lacks transparency. The city does keep a public database of MPD misconduct payouts.</p>
 
-<div class="download"><a href="../store/conduct/mpd_misconduct.csv">Download Minneapolis misconduct payout data, 1998-present</a></div>
+<div class="download"><a href="../store/conduct/mpd_misconduct.csv">Download Minneapolis misconduct payout data, 1998-2023</a></div>
 
 <h4>Links</h4>
 <ul>
     <li><a href="https://opendata.minneapolismn.gov/datasets/7f54c138c7d745618453fc9f4048d8f1_0/explore">Minneapolis Officer Conduct Data</a></li>
     <li><a href="https://www.minneapolismn.gov/government/government-data/datasource/office-of-police-conduct-review-dashboard/">Minneapolis Office of Police Conduct Review Dashboard</a></li>
+    <li><a href="https://www.minneapolismn.gov/government/government-data/datasource/police-complaints-count-dashboard/">Minneapolis Count of Police Complaints Dashboard</a></li>
     <li><a href="https://www2.minneapolismn.gov/government/departments/civil-rights/opcr/civilian-oversight-history/">History of civilian oversight of law enforcement in Minneapolis</a></li>
 </ul>
 
